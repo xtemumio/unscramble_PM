90c0e5cf3ed9327ae865a57eb9753f4d265fc4cf
paolo <mpaolo025@gmail.com>
2023-09-25 17:03:35 +0200

    first commint

--- app/src/main/java/com/example/android/unscramble/MainActivity.kt
+++ app/src/main/java/com/example/android/unscramble/MainActivity.kt
@@ -19,9 +19,6 @@ package com.example.android.unscramble
 import android.os.Bundle
 import androidx.appcompat.app.AppCompatActivity
 
-/**
- * Creates an Activity that hosts the Game fragment in the app
- */
 class MainActivity : AppCompatActivity() {
 
     override fun onCreate(savedInstanceState: Bundle?) {
